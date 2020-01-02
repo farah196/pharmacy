5c306da68313e4f3dad4ea3364c541020a58c266
farah <farah@for9a.com>
2020-01-02 22:58:35 +0200

    Initial Commit

---
+++ pharmacy/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  pharmacy
+//
+//  Created by Farah on 1/2/20.
+//  Copyright © 2020 Farah. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pharmacy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  pharmacy
+//
+//  Created by Farah on 1/2/20.
+//  Copyright © 2020 Farah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
